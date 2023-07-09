5f63fd75e1eb1d3248a1c9c6c126a7baaa48fea3
Kevin H <main.share.us@gmail.com>
2023-07-09 00:10:06 -0700

    Updated .gitignore, working on automated calendar send

--- send_email.py
+++ send_email.py
@@ -1,37 +1,52 @@
 import datetime
 import numpy as np
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2 import service_account
+from dateutil.relativedelta import relativedelta, MO
+import utils
+
+import os
+
+
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from dateutil.relativedelta import relativedelta, MO
-import os
-import utils
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 def send_invites(email, np_array, start_date, calendar_name, calendar_location, calendar_description):
     # Ensure start_date is a Monday
     if start_date.weekday() != 0:
         start_date = start_date + relativedelta(weekday=MO)
 
+    # creds = service_account.Credentials.from_service_account_file(
+    #     "credentials.json", scopes=SCOPES
+    # )
+
+    # service = build('calendar', 'v3', credentials=creds)
+
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', utils.SCOPE)
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', utils.SCOPE)
+                'local_oauth_secret.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
-
+    
     service = build('calendar', 'v3', credentials=creds)
+
 
     for i in range(np_array.shape[0]):
         j = 0
@@ -66,11 +81,15 @@ def send_invites(email, np_array, start_date, calendar_name, calendar_location, 
                 }
 
                 # Call the Calendar API
-                event = service.events().insert(calendarId='primary', body=event).execute()
+                event = service.events().insert(calendarId='primary', sendNotifications=True, body=event).execute()
             j += 1
             
 
 if __name__ == "__main__":
-    timeslots = np.random.randint(2, size=(5, 12))  # Random example array
+    timeslots = np.zeros((5, 12))  # Random example array
+    timeslots[1][1] = 1
+    timeslots[1][2] = 1
+
+    monday_date = utils.nearest_future_monday('2023-07-17')
     print(timeslots)
-    send_invites('cyrushung822@berkeley.edu', timeslots, '2023-06-26', "OH time", "warren", "damn I love this OH")
+    send_invites('main.share.us@gmail.com', timeslots, monday_date, "OH time", "warren", "damn I love this OH")
